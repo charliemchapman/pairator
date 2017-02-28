4d95b30160553eaca521a170cbbc8dc1cf6b3be4
Charlie Chapman <charliemchapman@gmail.com>
2017-02-27 22:09:51 -0600

    added c3 graphing and some pie graphs on the graph component

--- pairator_web/src/components/graphs.js
+++ pairator_web/src/components/graphs.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getTeam, getUser, getStation } from '../pairatorApi';
-import { setTeam } from '../actions/index';
+import { setTeam, addUser, addStation } from '../actions/index';
+import C3Chart from 'react-c3js';
+import 'c3/c3.css';
 
 class Graphs extends Component {
   componentWillMount(){
@@ -36,24 +38,83 @@ class Graphs extends Component {
     })
   }
 
+  getChart(userId, users, pairHistory, index){
+    const mainUserName = users[userId] ? users[userId].name : '';
+    const columns = { alone: 0 };
+    Object.keys(users).forEach(u=>{
+      if (users[u].name){
+        columns[users[u].name] = 0
+      }
+    });
+
+    pairHistory.forEach(history=>{
+      history.pairs.forEach(pair=>{
+        if (pair.users.includes(userId)){
+          pair.users.forEach(user=>{
+            const userName = users[user] ? users[user].name : '';
+            if (userName && user != userId){
+            if (!columns[userName]) {columns[userName] = 0}
+              columns[userName] += 1;
+            }
+          })
+          if (pair.users.length === 1){
+            columns.alone += 1;
+          }
+        }
+      })
+    })
+
+    const chartData = {
+      columns: [],
+      type: 'pie',
+      color: {
+        pattern: ['#d62728', '#2ca02c', '#9467bd', '#1f77b4', '#8c564b', '#ff7f0e']
+      }
+    }
+    Object.keys(columns).forEach(c=>chartData.columns.push([c, columns[c]]));
+
+    return (
+      <div className="userPieChart" key={index}>
+        <h2>{mainUserName}</h2>
+        <C3Chart data={chartData} />
+      </div>
+    );
+  }
+
+
   render() {
+    let charts;
+    charts = Object.keys(this.props.users).filter(x=>x!='027be6f0-7eeb-4a95-9e1c-c57afa259857').map((user,i)=>{
+      return this.getChart(user, this.props.users, this.props.team.pairHistory, i)
+    })
+
     return (
       <div>
         <h2>
           { this.props.team.name }
         </h2>
+        <div className="graph">
+          {charts}
+        </div>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state, ownProps)=>{
-  return { team: state.team, stations: state.stations, users: state.users, bench: state.bench }
+  return {
+    team: state.team,
+    stations: state.stations,
+    users: state.users,
+    bench: state.bench
+  }
 }
 
 const mapDispatchToProps = (dispatch, ownProps)=>{
   return {
-    setTeam: (team)=>dispatch(setTeam(team))
+    setTeam: (team)=>dispatch(setTeam(team)),
+    addStation: (station)=>dispatch(addStation(station)),
+    addUser: (user)=>dispatch(addUser(user))
   }
 }
 
